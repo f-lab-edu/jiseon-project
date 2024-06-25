129b469e3788cac1bfeecadf0f8bf678e67be3f6
eazyseon <eazyseon@gmail.com>
2024-06-25 20:22:08 +0900

    초기세팅 및 dao 만들기

---
+++ jiseon/src/models/user.ts
@@ -0,0 +1,11 @@
+export interface ISignUpUser {
+  email: string;
+  password: string;
+  nickname: string;
+  contact_info: string;
+}
+
+export interface ILoginUser {
+  email: string;
+  password: string;
+}
